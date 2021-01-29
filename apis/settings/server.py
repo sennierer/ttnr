262cde2b760837f1e7b0acc824516b8512680273
Matthias Schlögl <m.schloegl@gmail.com>
2021-01-29 14:45:47 +0100

    initial commit

---
+++ apis/settings/server.py
@@ -0,0 +1,36 @@
+from .base import *
+import re
+import dj_database_url
+import os
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "xh0hdptp4mb$h_qje*j=wp9hblb6pcg2fbwdq5yod10=2k)e0-"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+APIS_LIST_VIEWS_ALLOWED = True
+APIS_DETAIL_VIEWS_ALLOWED = True
+BIRTH_REL_NAME = "place of birth"
+DEATH_REL_NAME = "place of death"
+APIS_BASE_URI = "https://thinktanknetworkresearch.net/"
+
+
+ALLOWED_HOSTS = ["thinktanknetworkresearch.net"]
+# You need to allow '10.0.0.0/8' for service health checks.
+
+
+DEBUG = True
+DEV_VERSION = True
+
+SPECTACULAR_SETTINGS["COMPONENT_SPLIT_REQUEST"] = True
+SPECTACULAR_SETTINGS["COMPONENT_NO_READ_ONLY_REQUIRED"] = True
+
+DATABASES = {}
+
+DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+
+LANGUAGE_CODE = "de"
+
+
+APIS_RELATIONS_FILTER_EXCLUDE += ["annotation", "annotation_set_relation"]
---
+++ apis/urls.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.urls import path
+
+from apis_core.apis_entities.api_views import GetEntityGeneric
+
+urlpatterns = [
+    url(r"^apis/", include("apis_core.urls", namespace="apis")),
+    url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path(
+        r"entity/<int:pk>/", GetEntityGeneric.as_view(), name="GetEntityGenericRoot"
+    ),
+    url(r"^admin/", admin.site.urls),
+    url(r"^info/", include("infos.urls", namespace="info")),
+    url(r"^", include("webpage.urls", namespace="webpage")),
+]
+
+if "transkribus" in settings.INSTALLED_APPS:
+    urlpatterns = urlpatterns + [
+        url(r"^transkribus/", include("transkribus.urls")),
+    ]
+
+if "apis_bibsonomy" in settings.INSTALLED_APPS:
+    urlpatterns.append(
+        url(r"^bibsonomy/", include("apis_bibsonomy.urls", namespace="bibsonomy"))
+    )
+
+handler404 = "webpage.views.handler404"
